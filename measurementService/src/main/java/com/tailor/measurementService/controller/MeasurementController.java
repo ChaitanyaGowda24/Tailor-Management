5d3f90cb96e65f318527e0301bd9138003ec0833
VIGNAN DOGJU <vignandigoju@gmail.com>
2025-01-22 12:29:03 +0530

    Adjusted Request Mapping

--- measurementService/src/main/java/com/tailor/measurementService/controller/MeasurementController.java
+++ measurementService/src/main/java/com/tailor/measurementService/controller/MeasurementController.java
@@ -4,12 +4,10 @@ import com.tailor.measurementService.dto.ResponseDto;
 import com.tailor.measurementService.entity.Measurement;
 import com.tailor.measurementService.service.MeasurementService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/measurements")
 public class MeasurementController {
     @Autowired
     MeasurementService measurementService;
